c947487619ae5f63ebb85254aa1582a82f982f18
Mickey Mouse <mike.feith@gmail.com>
2019-04-23 12:53:26 +0200

    verder gewerkt aan puntenverzamlaar

--- Mike.py
+++ Mike.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-def puntenverzamelaar(puntfile,naam):
+
+def GradenNaarMeters(meters):
+   graden=(meters/30.92)/3600
+   return graden
+
+def puntenverzamelaar(puntfile,meters):
     #laad de punten file in
     puntenfile = pd.read_csv(puntfile)
     # id file
-    idfile = pd.dataframe()
-    for naam in idfile:
-        for punt in puntenfile:
-            maxlon = punt +
-            minlon =
-            maxlan =
-            minlan =
+    idfile = ()
+    for row in puntenfile:
+        maxlon = row('pnt_lon') + GradenNaarMeters(meters)
+        minlon = row('pnt_lon') - GradenNaarMeters(meters)
+        maxlan = row('pnt_lat') + GradenNaarMeters(meters)
+        minlan = row('pnt_lat') - GradenNaarMeters(meters)
+        id= pnt_id
+        idfile.append(id,maxlon,minlon,maxlan,minlan)
+    return print(idfile)
+
+puntenverzamelaar('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv',50)
