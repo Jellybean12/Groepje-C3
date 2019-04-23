ba8ee5313429b3195e0a761ef57eff25dd4aeefc
flier36 <46922416+flier36@users.noreply.github.com>
2019-04-23 09:52:50 +0200

    Update hallo123.py

--- hallo123.py
+++ hallo123.py
@@ -4,18 +4,46 @@ import re
 
 
 def bestand(bestandsnaam):
-    bestandsnaam = bestandsnaam.replace("_", " ")
-    print(bestandsnaam)
-    if 'rsat2' in bestandsnaam:
-        print("rsat2")
-    elif 'env' in bestandsnaam:
-        print('env')
-    elif 'rsat3' in bestandsnaam:
-        print('rsat3')
+    nieuwbestand = bestandsnaam.replace("_", " ")
+    return nieuwbestand
 
-bestand('prov_overijssel_eindhoven_rsat3')
-#txt = 'prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten'
+def get_type(invoer):
+        if 'asc' in invoer:
+            if 'ds' in invoer:
+                if 'hoge' in invoer:
+                    return 'HDA'
+                else:
+                    return 'LDA'
+            elif 'hoge' in invoer:
+                return 'HPA'
+            else:
+                return 'LPA'
+        elif 'ds' in invoer:
+            if 'hoge' in invoer:
+                return 'HDD'
+            else:
+                return 'LDD'
+        elif 'hoge' in invoer:
+            return 'HPD'
+        else:
+            return 'LPD'
 
+
+def get_satalliet(invoer):
+    if 'rsat2' in invoer:
+        return 'rsatxf'
+    elif 'rsat3' in invoer:
+        return 'rsat3'
+    else:
+        return 'env'
+
+#bestand('prov_overijssel_eindhoven_env_dsc_v2_ds_punten')
+csv = 'prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten'
+hoi = bestand(csv)
+doei = get_type(hoi)
+print(doei)
+hallo = get_satalliet(hoi)
+print(hallo)
 #txt = txt.replace("_", " ")
 #print(txt)
 
