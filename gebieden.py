375e1ebf2716e8a6a2ab4060620cc8a204267967
flier36 <46922416+flier36@users.noreply.github.com>
2019-04-24 11:57:48 +0200

    Update gebieden.py

--- gebieden.py
+++ gebieden.py
@@ -12,7 +12,7 @@ def schoon(naam):
     return data
 
 #Functie aanroepen met csv bestand
-voorbeeld = schoon('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv')
+voorbeeld = schoon('prov_overijssel_groningen_rsat2_asc_xf_v2_ds_lage_punten.csv')
 
 #Condities aangeven
 conditie = [
@@ -30,17 +30,21 @@ conditie = [
     (voorbeeld['pnt_lat'] >= 52.32) & (voorbeeld['pnt_lat'] <= 52.38) & (voorbeeld['pnt_lon'] >= 6.8) & (voorbeeld['pnt_lon'] <= 6.9),
     #Espelose broek
     (voorbeeld['pnt_lat'] >= 52.28) & (voorbeeld['pnt_lat'] <= 52.33) & (voorbeeld['pnt_lon'] >= 6.3) & (voorbeeld['pnt_lon'] <= 6.4),
+    #Sint Jans Klooster
+    (voorbeeld['pnt_lat'] >= 52.63) & (voorbeeld['pnt_lat'] <= 52.71) & (voorbeeld['pnt_lon'] >= 5.8) & (voorbeeld['pnt_lon'] <= 6.2),
+    #Rodenmors
+    (voorbeeld['pnt_lat'] >= 52.36) & (voorbeeld['pnt_lat'] <= 52.42) & (voorbeeld['pnt_lon'] >= 7.0) & (voorbeeld['pnt_lon'] <= 7.1),
     #Holten
     (voorbeeld['pnt_lat'] >= 52.24) & (voorbeeld['pnt_lat'] <= 52.33) & (voorbeeld['pnt_lon'] >= 6.4) & (voorbeeld['pnt_lon'] <= 6.5)
 ]
 
-keuze = ['Engelse Werk', 'Hammerflier', 'Manderveen', 'Wierden', 'Hasselo', 'Weerselo', 'Espelose Broek', 'Holten']
+keuze = ['Engelse Werk', 'Hammerflier', 'Manderveen', 'Wierden', 'Hasselo', 'Weerselo', 'Espelose Broek', 'Holten', 'SintJansKlooster', 'Rodenmors']
 voorbeeld['Gebied'] = np.select(conditie, keuze, default=0)
 
 #Bestand opslaan als excel
-writer = ExcelWriter('eindhoven_rsat2_asc_xf_v2_ds_hoog_gebieden.xlsx')
-voorbeeld.to_excel(writer, 'Sheet1', index=False)
-writer.save()
+#writer = ExcelWriter('eindhoven_rsat2_asc_xf_v2_ds_hoog_gebieden.xlsx')
+#voorbeeld.to_excel(writer, 'Sheet1', index=False)
+#writer.save()
 
 #Scatterplot maken
 scatter = sns.scatterplot(x='pnt_lon', y='pnt_lat', hue='Gebied', data=voorbeeld, legend='full')
