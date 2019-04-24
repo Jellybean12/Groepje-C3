0aa750127be0f45b8c374115839f9ce587bd86bf
flier36 <46922416+flier36@users.noreply.github.com>
2019-04-24 11:16:49 +0200

    bestand

--- kmeans.py
+++ kmeans.py
@@ -1,37 +1,47 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 import seaborn as sns
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
+#Functie voor het opschonen van de data
 def schoon(naam):
     data = pd.read_csv(naam)
-    data = data[['pnt_lat', 'pnt_lon']]
+    data = data[['pnt_id', 'pnt_lat', 'pnt_lon']]
     return data
 
+#Functie aanroepen met csv bestand
+voorbeeld = schoon('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv')
 
-data = schoon('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv')
+#Condities aangeven
+conditie = [
+    #Engelse Werk
+    (voorbeeld['pnt_lat'] >= 52.46) & (voorbeeld['pnt_lat'] <= 52.54) & (voorbeeld['pnt_lon'] >= 6.0) & (voorbeeld['pnt_lon'] <= 6.2),
+    #Hammerflier
+    (voorbeeld['pnt_lat'] >= 52.44) & (voorbeeld['pnt_lat'] <= 52.5) & (voorbeeld['pnt_lon'] >= 6.5) & (voorbeeld['pnt_lon'] <= 6.7),
+    #Manderveen
+    (voorbeeld['pnt_lat'] >= 52.42) & (voorbeeld['pnt_lat'] <= 52.48) & (voorbeeld['pnt_lon'] >= 6.7) & (voorbeeld['pnt_lon'] <= 6.9),
+    #Wierden
+    (voorbeeld['pnt_lat'] >= 52.32) & (voorbeeld['pnt_lat'] <= 52.41) & (voorbeeld['pnt_lon'] >= 6.5) & (voorbeeld['pnt_lon'] <= 6.7),
+    #Hasselo
+    (voorbeeld['pnt_lat'] >= 52.28) & (voorbeeld['pnt_lat'] <= 52.32) & (voorbeeld['pnt_lon'] >= 6.7) & (voorbeeld['pnt_lon'] <= 6.9),
+    #Weerselo
+    (voorbeeld['pnt_lat'] >= 52.32) & (voorbeeld['pnt_lat'] <= 52.38) & (voorbeeld['pnt_lon'] >= 6.8) & (voorbeeld['pnt_lon'] <= 6.9),
+    #Espelose broek
+    (voorbeeld['pnt_lat'] >= 52.28) & (voorbeeld['pnt_lat'] <= 52.33) & (voorbeeld['pnt_lon'] >= 6.3) & (voorbeeld['pnt_lon'] <= 6.4),
+    #Holten
+    (voorbeeld['pnt_lat'] >= 52.24) & (voorbeeld['pnt_lat'] <= 52.33) & (voorbeeld['pnt_lon'] >= 6.4) & (voorbeeld['pnt_lon'] <= 6.5)
+]
 
-def kmeans(data, clusters):
-    model = KMeans(n_clusters=clusters)
-    kmeansmodel = model.fit(data)
-    data['cluster'] = kmeansmodel.labels_
-    return data
+keuze = ['Engelse Werk', 'Hammerflier', 'Manderveen', 'Wierden', 'Hasselo', 'Weerselo', 'Espelose Broek', 'Holten']
+voorbeeld['Gebied'] = np.select(conditie, keuze, default=0)
 
+#Bestand opslaan als excel
+writer = ExcelWriter('eindhoven_rsat2_asc_xf_v2_ds_hoog_gebieden.xlsx')
+voorbeeld.to_excel(writer, 'Sheet1', index=False)
+writer.save()
 
-
-#print(data.head)
-voorbeeld = kmeans(schoon('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv'), 8)
-data1 = voorbeeld.iloc[:, :18294]
-data2 = voorbeeld.iloc[:, 18294:]
-
-print(data2.info())
-
-#scatter = sns.scatterplot(x='pnt_lon', y='pnt_lat', hue='cluster', data=data1, size='cluster', legend='full')
-#plt.show(scatter)
-#plt.show(var)
-#plt.scatter(voorbeeld['pnt_lon'], voorbeeld['pnt_lat'], alpha=0.1)
-#plt.show()
-
-#print(data('prov_overijssel_eindhoven_rsat2_asc_xf_v2_ds_hoge_punten.csv'))
-#scatterplot
-#plt.scatter(df['pnt_lon'], df['pnt_lat'], alpha=0.1)
+#Scatterplot maken
+scatter = sns.scatterplot(x='pnt_lon', y='pnt_lat', hue='Gebied', data=voorbeeld, legend='full')
+plt.show(scatter)
