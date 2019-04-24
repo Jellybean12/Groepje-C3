03cc5f231ec6922e4c43f531d166b0f3a7ca0129
Mickey Mouse <mike.feith@gmail.com>
2019-04-24 10:44:17 +0200

    commit working

--- Mike.py
+++ Mike.py
@@ -9,19 +9,8 @@ def GradenNaarMeters(meters):
    return graden
 
 #functie voor het verzamelen van punten
-def puntenverzamelaar2 (dataset,meters):
-    eindlist= []
-    #een for loop zodat alle rows in een dataframe wordt gebruikt om meer
-    #for row in dataset:
-        #boorid = eindlist["BoorID"].append(dataset.loc[:, 'Boring'])
-       #locatie = eindlist["Locatie"].append(dataset.loc[:, 'Locatie'])
-        #maxlon = eindlist.append({"MaxLon" : (dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters)})
-    maxlon = eindlist.append({'MaxLon': dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters)})
-        #minlon = eindlist["MinLon"].append(dataset.loc[:, 'pnt_lon'] - GradenNaarMeters(meters))
-        #maxlat = eindlist["MaxLat"].append(dataset.loc[:, 'pnt_lat'] + GradenNaarMeters(meters))
-        #minlat = eindlist["MinLat"].append(dataset.loc[:, 'pnt_lat'] - GradenNaarMeters(meters))
-    return print(eindlist)
-#print(puntenverzamelaar2(inactieveputtendf, 50))
+
+
 #if statement om te kijken of een dieptemetingpunt in range is met de zoutcaverne
 punten = []
 #for row in eindlist:
@@ -30,29 +19,55 @@ punten = []
 
 ###############################
 inactieveputtendf= pd.read_csv('inactieve_putten - page 1 2.csv',sep=',',)
+#print(inactieveputtendf)
+#def puntenverzamelaar2 (dataset,meters):
+#   locatie = []
+#   maxlon = []
+#   minlon= []
+#  maxlat= []
+#   minlat= []
+   #een for loop zodat alle rows in een dataframe wordt gebruikt om meer
+#   for row in dataset:
+#       locatie.append(dataset.loc[:, 'Locatie'])
+#       maxlon.append(dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters))
+#       minlon.append(dataset.loc[:, 'pnt_lon'] - GradenNaarMeters(meters))
+ #      maxlat.append(dataset.loc[:, 'pnt_lat'] + GradenNaarMeters(meters))
+#       minlat.append(dataset.loc[:, 'pnt_lat'] - GradenNaarMeters(meters))
+
+#   eindlist= pd.DataFrame(columns=[["Locatie","MaxLat","MinLat","MaxLon","MinLon"]])
+
+#for row in
+#   eindlist["Locatie"]=locatie
+#   eindlist["MaxLat"]=maxlat
+#   eindlist["MinLat"]=minlat
+#   eindlist["MaxLon"]=maxlon
+#   eindlist["MinLon"]=minlon
+
+
+#   return pd.DataFrame(eindlist)
+
+
+
 
 def puntenverzamelaar2 (dataset,meters):
-   locatie = []
-   maxlon = []
-   minlon= []
-   maxlat= []
-   minlat= []
-   #een for loop zodat alle rows in een dataframe wordt gebruikt om meer
-   for row in dataset:
-       locatie.append(dataset.loc[:, 'Locatie'])
-       maxlon.append(dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters))
-       minlon.append(dataset.loc[:, 'pnt_lon'] - GradenNaarMeters(meters))
-       maxlat.append(dataset.loc[:, 'pnt_lat'] + GradenNaarMeters(meters))
-       minlat.append(dataset.loc[:, 'pnt_lat'] - GradenNaarMeters(meters))
-
-   eindlist= pd.DataFrame(columns=[["Locatie","MaxLat","MinLat","MaxLon","MinLon"]])
-
-for row in
-   eindlist["Locatie"]=locatie
-   eindlist["MaxLat"]=maxlat
-   eindlist["MinLat"]=minlat
-   eindlist["MaxLon"]=maxlon
-   eindlist["MinLon"]=minlon
+    #eindlist= []
+    eindlist= pd.read_csv('csv boor.csv')
+    #een for loop zodat alle rows in een dataframe wordt gebruikt om meer
+    for row in dataset:
+        eindlist["BoorID"] = dataset.loc[:, 'Boring']
+        eindlist["Locatie"] = dataset.loc[:, 'Locatie']
+        eindlist["MaxLon"] = dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters)
+   # maxlon = eindlist.append({'MaxLon': dataset.loc[:, 'pnt_lon'] + GradenNaarMeters(meters)})
+        eindlist["MinLon"] = dataset.loc[:, 'pnt_lon'] - GradenNaarMeters(meters)
+        eindlist["MaxLat"] = dataset.loc[:, 'pnt_lat'] + GradenNaarMeters(meters)
+        eindlist["MinLat"] = dataset.loc[:, 'pnt_lat'] - GradenNaarMeters(meters)
+    return print(eindlist)
+print(puntenverzamelaar2(inactieveputtendf, 50))
 
 
-   return pd.DataFrame(eindlist)
+
+
+
+
+
+
