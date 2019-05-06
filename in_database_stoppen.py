3119e1fd4f8df881c607580777d05aa9a8bba7b0
Ralphine <r.deroo99@gmail.com>
2019-05-06 11:19:38 +0200

    De inactieve en actieve putten die helemaal klaar zijn

---
+++ in_database_stoppen.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm import tqdm
+
+def chunker(seq, size):
+    # from http://stackoverflow.com/a/434328
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+def insert_with_progress(df):
+    #maak connectie met database
+    con = create_engine('postgresql://postgres:Welkom01!@10.30.1.10:5432/POC')
+    # set chunksize
+    chunksize = int(len(df) / 10)
+    with tqdm(total=len(df)) as pbar:
+        for i, cdf in enumerate(chunker(df, chunksize)):
+            # chunked df toevoegen aan database in tabel boor_locatie
+            cdf.to_sql('boor_locatie', con=con, if_exists='append', index=False)
+            pbar.update(chunksize)
+
+
+df_inactief = pd.read_csv("inactieve_putten.csv")
+
+insert_with_progress(df_inactief)
