7c3e439852a447e09a51b6cdce7d5f19cb1bf232
Mickey Mouse <mike.feith@gmail.com>
2019-05-16 12:53:56 +0200

    push

--- arima2.py
+++ arima2.py
@@ -4,14 +4,25 @@ from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+from math import sqrt
 
 def parser(x):
     return datetime.strptime('190 ' +x, '%Y-%m')
 
-series = read_csv('metingenarima.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+series = pd.read_csv('metingenarima.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
 series = pd.DataFrame(series)
 series['meting'] = series['meting'].multiply(1000)
-#series = series.head(1000)
+#series = series.head(100)
+#print(series.head())
+y = series
+
+# The 'MS' string groups the data in buckets by start of the month
+y = y['meting'].resample('MS').mean()
+
+# The term bfill means that we use the value before filling in missing values
+y = y.fillna(y.bfill())
+
+series = y
 X = series.values
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
@@ -26,7 +37,7 @@ for t in range(len(test)):
     obs = test[t]
     history.append(obs)
     print('predicted=%f, expected=%f' % (yhat, obs))
-error = mean_squared_error(test, predictions)
+error = sqrt(mean_squared_error(test, predictions))
 print('Test MSE: %.3f' % error)
 # plot
 pyplot.plot(test)
