ae08d41aa830a2325f3cb1704c77ef9727782878
Ralphine <r.deroo99@gmail.com>
2019-05-17 14:18:45 +0200

    Fien check

---
+++ FienCheckRichtingscoefficient.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+######## WORK IN PROGRESS ###########
+
+
+def richtingscoefficient(pnt, dfmeting = 0):
+    """___________"""
+    # Wanneer de meting 0 is (geen meting mee gegeven), maak dan een connectie met de database en maak een dataframe met een query.
+    if dfmeting == 0 :
+        url = 'postgresql://postgres:Welkom01!@10.30.1.10:5432/POC'
+        query = """select * from meting where pnt_id in ('""" + pnt + """')"""
+        df = create_dataframe_from_query(url, query)
+    # Anders is df de meegegeven dataframe
+    else :
+        df = dfmeting
+
+    df['date_ordinal'] = pd.to_datetime(df['datum']).apply(lambda date: date.toordinal())
+
+    df.plot(x='date_ordinal', y='meting')
+    # plt.show()
+
+    #### richtingscoefficient gedeelte ####
+    # (X*Y).mean(axis=1) - X.mean()*Y.mean(axis=1)) / ((X**2).mean() - (X.mean())**2)
+    x = df['date_ordinal']
+    y = df['meting']
+    print(df['meting'])
+
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    slopeinmeters = slope * 1000
+    #print(slopeinmeters * 365)
+    # print('de verzaking is: ',slopeinmeters,'CM')
+    #plt.plot(x, y, 'o', label='original data')
+    #plt.plot(x, intercept + slope * x, 'r', label='fitted line')
+    #plt.legend()
+    return print('De gemiddelde stijging/daling van punt ',pnt,' is : ',slopeinmeters*365,'mm per jaar  ')
+
+def create_dataframe_from_query(url, query):
+    """Maakt een connectie met een database met de meegegeven url. Met de query wordt een dataframe gemaakt."""
+    engine = create_engine(url)
+    df = pd.read_sql(query, engine)
+    return df
+
---
+++ program.py
@@ -0,0 +1,4 @@
+import richtingscoefficient_slope as rich
+
+rich.richtingscoefficient('L346887P7846')
+
