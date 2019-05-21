ac0ab529761a4d46a76767762d3a5570e264ce10
Ralphine <r.deroo99@gmail.com>
2019-05-21 10:19:31 +0200

    Update in_database_stoppen.py
    Een filename kun je nu in die functie stoppen

--- in_database_stoppen.py
+++ in_database_stoppen.py
@@ -6,9 +6,10 @@ def chunker(seq, size):
     # from http://stackoverflow.com/a/434328
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-def insert_with_progress(df):
+def insert_with_progress(filename):
     #maak connectie met database
     con = create_engine('postgresql://postgres:Welkom01!@10.30.1.10:5432/POC')
+    df = pd.read_csv(filename)
     # set chunksize
     chunksize = int(len(df) / 10)
     with tqdm(total=len(df)) as pbar:
@@ -17,7 +18,4 @@ def insert_with_progress(df):
             cdf.to_sql('boor_locatie', con=con, if_exists='append', index=False)
             pbar.update(chunksize)
 
-# is nu leeg, want je moet niet zomaar weer in de database kunnen doen
-df_inactief = pd.read_csv("")
-
-insert_with_progress(df_inactief)
+  
