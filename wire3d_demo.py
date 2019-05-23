09100f883aa037828894a3f2a1c4c3dba8b3c223
Thommydavries <Thomasdavries@hotmail.com>
2019-05-23 10:35:54 +0200

    h

--- wire3d_demo.py
+++ wire3d_demo.py
@@ -1,22 +1,50 @@
-'''
-=================
-3D wireframe plot
-=================
+import pandas as pd
+from sqlalchemy import create_engine
+import datetime
+import numpy as np
+import re
+from matplotlib.pylab import rcParams
+import sqlite3
+import seaborn as sns; sns.set()
+from tqdm import tqdm
+rcParams['figure.figsize'] = 12, 10
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
 
-A very basic demonstration of a wireframe plot.
-'''
 
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+
+series = read_csv('satallbor475.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+series = pd.DataFrame(series)
+series['meting'] = series['meting'].multiply(1000)
+series = series.head(1000)
+X = series.values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+    model = ARIMA(history, order=(5,1,0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
+    history.append(obs)
+    print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+from statsmodels.tsa.arima_model import ARIMA
 
-# Grab some test data.
-X, Y, Z = axes3d.get_test_data(0.05)
-
-# Plot a basic wireframe.
-ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
-
-plt.show()
+model = ARIMA(series.meting, order=(2, 2, 10))
+results_ARIMA = model.fit(disp=-1)
