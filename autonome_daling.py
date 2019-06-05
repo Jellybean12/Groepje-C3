38b0a07494ef74613241fa43c96e1baafa214e90
Ralphine <r.deroo99@gmail.com>
2019-06-05 12:51:01 +0200

    Update autonome_daling.py
    Autonome daling is nu beter, doet het weer als je hem runt.

--- autonome_daling.py
+++ autonome_daling.py
@@ -1,11 +1,8 @@
-import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
-import sqlite3
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
-import datetime
-from tqdm import tqdm
+import gemiddelde_metingen as gem_met
 
 def get_metingen(satid):
     """Met de functie getmetingen wordt er een select statement uitgevoerd met het geselecteerde sat_id, deze functie is gebruikt
@@ -28,7 +25,6 @@ def get_metingen(satid):
 def line_plot_average_half_year(df, locatie):                                                                                                                            
     """Deze functie maakt een lijn plot van een DataFrame. Gebruik een df die aangemaakt is door de functie Gebruikende datum_ordinal als x en meting als y"""      
     #data = per_unique_point_average_half_year(df)                                                                                                                   
-    #plt.figure(figsize=(w,h))  
     title = 'Autonome daling '+str(locatie)+''
     ax = sns.lineplot(                                                                                                                                              
         data=df,                                                                                                                                                  
@@ -40,18 +36,15 @@ def line_plot_average_half_year(df, locatie):
     ax.set_title(title)    
     fig = ax.get_figure()
     fig.set_size_inches(16, 10)
-    fig.savefig('C:\\Users\\Proof of Concept\\Pictures\\autonome_daling_ '+str(locatie)+'.png')
-
-
-import gemiddelde_metingen as gem_met
+    plt.show()
 
 url = 'postgresql://postgres:Welkom01!@10.30.1.10:5432/POC'
-query = """SELECT * FROM meting WHERE pnt_id IN (SELECT pnt_id FROM pnt_locatie WHERE locatie = 'Hammerflier' LIMIT 1000)"""
+query = """SELECT * FROM meting WHERE pnt_id IN (SELECT pnt_id FROM pnt_locatie WHERE locatie = 'Hammerflier' LIMIT 100)"""
 df = gem_met.create_dataframe_from_query(url, query)
 
 new_df = gem_met.per_unique_point_average_half_year(df)
 
-line_plot_average_half_year(new_df, 'geen idee', 'ook geen idee')
+line_plot_average_half_year(new_df, 'Hammerflier LIMIT 100')
 
 
 
